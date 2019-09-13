266f603cd8f2448ef86901c9de16d8aa299a8c91
Mysh3ll <peacefulloijkn@gmail.com>
2019-09-13 11:30:49 +0200

    Basic template ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
